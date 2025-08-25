c07320d25dbeba705b12da2e7dc14b19107a6311
SamsShow <sakshamtyagi2008@gmal.com>
2025-08-25 13:08:31 +0530

    Basic stuff done

---
+++ WeatherDemo/View/WeatherView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherView.swift
+//  WeatherDemo
+//
+//  Created by Saksham Tyagi on 25/08/25.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherView()
+}
