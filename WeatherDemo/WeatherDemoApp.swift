b80d6b757a45637efc77a23d6be58e6ca6be0af9
SamsShow <sakshamtyagi2008@gmal.com>
2025-08-25 11:51:26 +0530

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Saksham Tyagi on 25/08/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
